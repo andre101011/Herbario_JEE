c55e2a267d6f98c71174126a37ef5890475130af
Daniel <dbonillag_1@uqvirtual.edu.co>
2019-07-12 17:12:42 -0500

    va a a dar conflicto pero me toca corregirlo así

--- proyectoHerbario/webHerbario/src/main/java/co/edu/uniquindio/AAAD/bean/SeguridadBean.java
+++ proyectoHerbario/webHerbario/src/main/java/co/edu/uniquindio/AAAD/bean/SeguridadBean.java
@@ -19,6 +19,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import co.edu.uniquindio.AAAD.correo.EnviarCorreo;
 import co.edu.uniquindio.AAAD.ejb.AdminEJB;
 import co.edu.uniquindio.AAAD.ejb.NegocioEJB;
 import co.edu.uniquindio.AAAD.persistencia.Persona;
@@ -65,6 +66,7 @@ public class SeguridadBean implements Serializable {
 			autenticado = true;
 
 		} else {
+			autenticado=false;
 			Util.mostrarMensaje("verifique las credencialesde acceso", "verifique las credenciales de acceso");
 		}
 
@@ -76,57 +78,13 @@ public class SeguridadBean implements Serializable {
 		Persona persona = adminEJB.buscarPersonaPorEmail(correo);
 		if (persona != null) {
 			String clave = persona.getClave();
-			enviarConGMail(correo, "Su contraseña es " + clave);
+			EnviarCorreo.enviarConGMail(correo, "Su contraseña es " + clave);
 			Util.mostrarMensaje("Revise su correo", "La contraseña de su cuenta fue enviada a su correo");
 		} else {
 			Util.mostrarMensaje("Correo no encontrado", "El correo ingresado no existe");
 		}
 	}
 
-	public void enviarConGMail(String destinatario, String mensaje) {
-		// Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el
-		// remitente tambien.
-		String remitente = "herbariouniquindioAAAD";
-		String clave = "herbario";
-		// Para la direccion nomcuenta@gmail.com
-		Properties props = System.getProperties();
-		props.put("mail.smtp.host", "smtp.gmail.com"); // El servidor SMTP de Google
-		props.put("mail.smtp.user", remitente);
-		props.put("mail.smtp.clave", clave); // La clave de la cuenta
-		props.put("mail.smtp.auth", "true"); // Usar autenticacion mediante usuario y clave
-		props.put("mail.smtp.starttls.enable", "true"); // Para conectar de manera segura al servidor SMTP
-		props.put("mail.smtp.port", "587"); // El puerto SMTP seguro de Google
-		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-
-		Session session = Session.getDefaultInstance(props);
-		MimeMessage message = new MimeMessage(session);
-		BodyPart texto = new MimeBodyPart();
-		MimeMultipart multiParte = new MimeMultipart();
-		try {
-			texto.setText(mensaje);
-
-			multiParte.addBodyPart(texto);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			message.setFrom(new InternetAddress(remitente));
-			message.addRecipients(Message.RecipientType.TO, destinatario);
-
-			message.setSubject("Recuperacion de contrasena Herbario Universidad del quindio:");
-			message.setText("prueba");
-			message.setContent(multiParte);
-
-			Transport transport = session.getTransport("smtp");
-			transport.connect("smtp.gmail.com", remitente, clave);
-
-			transport.sendMessage(message, message.getAllRecipients());
-			transport.close();
-		} catch (MessagingException me) {
-			me.printStackTrace();
-		}
-	}
 
 	/**
 	 * @return the usuario
